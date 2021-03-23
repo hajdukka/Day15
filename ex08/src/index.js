ff3c66efcb92f4dc34e727e10851b56656d05f6f
hajdukka <nerminhajdo@gmail.com>
2021-03-23 10:19:22 +0100

    ex08 ispravka

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,14 +1,6 @@
-const sum = (num1, num2) => {
-  "use strict";
-  if (num2 > 1) {
-    let zbir = num1 + num2;
-    return zbir;
-  } else {
-    num2 = 1;
-    let zbir = num1 + num2;
-    return zbir;
-  }
-};
+const sum = (num1, num2 = 1) => {
+  return num1 + num2;
+}
 console.log(sum(4, 6));
 console.log(sum(3));
 module.exports = sum;
